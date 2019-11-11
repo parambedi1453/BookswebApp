8c90819ab59b5f1cb6c1a83619f7818ca059d372
parambedi1453 <parambedi1453@gmail.com>
2019-11-11 17:50:52 +0530

    books

---
+++ routes/books.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/book')
+
+// ALL Books Route
+router.get('/' ,async (req,res) => {
+
+   
+   
+})
+
+// New Book route
+router.get('/new',(req,res)=> {
+   
+})
+
+// create Book Route
+// using async and await
+
+router.post('/',async (req,res)=>{
+   
+})
+
+
+module.exports = router
