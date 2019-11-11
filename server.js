ffa8125b7854486127a3cc4563c3a2b5b7012de6
parambedi1453 <parambedi1453@gmail.com>
2019-11-11 16:43:07 +0530

    authors commit

--- server.js
+++ server.js
@@ -6,14 +6,20 @@ if(process.env.NODE_ENV !=='production')
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
 
 const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
 
 app.set('view engine','ejs')
 app.set('views',__dirname+ '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit : '10mb' , extended:false}))
+// by mentioning the limit we are saying that when we are sending to file
+
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{
@@ -24,6 +30,11 @@ const db = mongoose.connection
 db.on('error', error => console.log(error)) // if error occures on conection
 db.once('open' , () => console.log('connected to Mongoose'))//runns once for checking the established connectiom
 
+
+
 app.use('/',indexRouter)
+app.use('/authors',authorsRouter)
+// will  be used as authors/  and authors/new
+
 // process.env.PORT is pory of the hosting platform elsr the local is 30000
 app.listen(process.env.PORT || 3000)
