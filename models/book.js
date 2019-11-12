64ed177640dd039e45b475ee7a976f52ba68b227
parambedi1453 <parambedi1453@gmail.com>
2019-11-12 08:40:25 +0530

    books

--- models/book.js
+++ models/book.js
@@ -35,6 +35,12 @@ const bookSchema = new mongoose.Schema({
     }
 })
 
+bookSchema.virtual('coverImagePath').get(function(){
+    if(this.coverImageName != null)
+    {
+        return path.join('/',coverImageBasePath,this.coverImageName)
+    }
+})
 module.exports = mongoose.model('Book',bookSchema)
 module.exports.coverImageBasePath = coverImageBasePath
 
--- routes/books.js
+++ routes/books.js
@@ -17,7 +17,18 @@ const imageMimeTypes = ['image/jpeg','image/png','images/gif']
 
 // ALL Books Route
 router.get('/' ,async (req,res) => {
-    res.send('All Books')
+  try{
+
+    const books = await Book.find({})
+    res.render('books/index',{
+      books : books,
+      searchOptions : req.query
+    })
+  }
+  catch{
+    res.redirect('/')
+  }
+   
    
 })
 
